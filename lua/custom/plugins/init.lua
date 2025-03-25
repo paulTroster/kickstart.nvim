486ab05f365e46d74b6292236bd9ba32a1650cd6
Paul Tröster <61591051+Synx-pT@users.noreply.github.com>
2025-03-25 12:19:01 +0100

    Added new color theme and neotree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -426,4 +426,29 @@ return {
       end,
     },
   },
+  {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Enable true color support
+      if vim.fn.has 'termguicolors' == 1 then
+        vim.o.termguicolors = true
+      end
+
+      -- Set background to light
+      vim.o.background = 'dark'
+
+      -- Global variables for Everforest
+      vim.g.everforest_background = 'hard' -- Options: 'hard', 'medium', 'soft'
+      vim.g.everforest_better_performance = 1 -- Optimize for speed
+      vim.g.everforest_enable_italic = true -- Enable italics
+
+      -- Load the colorscheme
+      vim.cmd.colorscheme 'everforest'
+
+      -- Lightline colorscheme (if using lightline)
+      vim.g.lightline = { colorscheme = 'everforest' }
+    end,
+  },
 }
