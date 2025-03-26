d24cd99f41997f3337810462a65927cadd023abe
Paul Tröster <61591051+Synx-pT@users.noreply.github.com>
2025-03-26 09:51:40 +0100

    Added git integration

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -477,4 +477,13 @@ return {
     end,
     dependencies = { { 'nvim-tree/nvim-web-devicons' } },
   },
+  {
+    'tanvirtin/vgit.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+    -- Lazy loading on 'VimEnter' event is necessary.
+    event = 'VimEnter',
+    config = function()
+      require('vgit').setup()
+    end,
+  },
 }
