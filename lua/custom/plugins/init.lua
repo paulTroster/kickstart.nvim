cff337eb5e733083d0036dd1d9c8c7a67c7360e8
Synx-pT <61591051+Synx-pT@users.noreply.github.com>
2025-10-14 13:32:23 +0200

    removed autpairs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,9 +1,8 @@
 -- Main plugin configuration file that imports all individual plugin configs
 return {
   -- Import all plugins from their individual files
-  require 'custom.plugins.dooing',
+  -- require 'custom.plugins.dooing',
   require 'custom.plugins.vim-be-good',
-  require 'custom.plugins.autopairs',
   require 'custom.plugins.copilot',
   -- require 'custom.plugins.neo-tree',
   require 'custom.plugins.everforest',
